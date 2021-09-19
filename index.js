174eb8621c0c9dd4158fca988a77c2daa463fd74
Adriana <adriana.calvomatos@gmail.com>
2021-09-19 00:29:21 +0000

    complete sum of nest arrays

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  if (array.length === 0) {
+    return;
+  }
+
+  let sum = 0;
+  for (let item of array) {
+    // recursive case
+    if (Array.isArray(item)) {
+      // if empty array
+      if (item.length === 0) {
+        sum += 0;
+      } else {
+        // if nested array
+        sum += sumItems(item);
+      }
+    } else {
+      // if not an array or we've reached the end of the nesting
+      sum += item;
+    }
+  }
+
+  return sum;
 }
 
 module.exports = sumItems;
